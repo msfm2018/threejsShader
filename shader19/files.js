b27e38667a8019c1418dfe5f2d2dcb436ea9275e
msfm2018 <msfm@163.com>
2023-11-15 13:58:00 +0800

    update

---
+++ shader19/files.js
@@ -0,0 +1,38 @@
+var files = {
+	"three": [
+		"three_borderEarth",
+		"three_outLine",
+		"three_shadow",
+		"three_frameSelect",
+		"three_customTerrain",
+		"three_waterBall",
+		"three_simpleGrayPass",
+		"three_autoScaleSprite",
+		"three_cityCircle",
+		"three_customPlaneGeometry",
+		"three_geojsonBuilding",
+		"three_raisingCode",
+		"three_centerLoader",
+		"three_sweepPass",
+		"three_circleSweepPass",
+		"three_depthFogPass",
+		"three_depthFogPass2",
+		"three_customClip",
+		"three_dissolve1",
+		"three_dissolve2",
+		"three_dissolve3",
+		"three_projTexture"
+	],
+	"canvas": [
+		"canvas_rose",
+		"canvas_flyLine(飞线)",
+		"canvas_pixelMouseEvent",
+		"canvas_waterBall"
+	],
+	"svg": [
+		"svg_dynamicCircle",
+	],
+	"webgl": [
+		"webgl_shadow"
+	]
+};
